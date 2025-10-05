c7a2712ce2ee45efa4cb03bf7a7713cebf2c8c08
SahilShameerDev <smsahilms346@gmail.com>
2025-10-05 13:45:57 +0530

    moon improved

--- Astro/src/pages/HomePage.jsx
+++ Astro/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+  import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ArrowRight } from 'lucide-react';
 import Moon from '../components/Moon';
