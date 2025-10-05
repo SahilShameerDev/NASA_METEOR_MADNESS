c2000ba99438f1a0c9b32de97b885dbb5878ccb1
Alwin-Sajan <alwinsajan225@gmail.com>
2025-10-05 11:38:57 +0530

    fix the dependence issue

--- Astro/src/components/Globe.jsx
+++ Astro/src/components/Globe.jsx
@@ -1,19 +1,19 @@
-import React from "react";
 import { Viewer, Entity } from "resium";
-import { Cartesian3, Color } from "cesium";
+import { Cartesian3,Color } from "cesium";
+// import Cesium from
 
 function Globe() {
   return (
-    <div className="h-[600px] w-full">
+    <div className="h-full w-full relative ">
       <Viewer full>
         <Entity
           name="Asteroid Impact"
           position={Cartesian3.fromDegrees(-74.006, 40.7128, 500000)} // lon, lat, alt
-          point={{ pixelSize: 10, color: Color.RED }} // ✅ Use imported Color
+          point={{ pixelSize: 10, color: Color.RED }}
         />
       </Viewer>
     </div>
   );
 }
 
-export default Globe;
+export default Globe
